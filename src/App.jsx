64830e526e183a401041b107733dbd2d5df846f1
akash <akashmanjhi1111198@gmail.com>
2024-10-23 15:55:44 +0530

    push app.js

--- src/App.jsx
+++ src/App.jsx
@@ -12,45 +12,47 @@ import Gps from './pages/products/Gps'
 import FuelMonitor from './pages/products/FuelMonitor'
 import Cctv from './pages/products/Cctv'
 import AssetTracking from './pages/products/AssetTracking'
-import VehicalTracking from './pages/products/VehicalTracking'
+// import VehicleTracking from './pages/products/VehicleTracking'
 import WasteTracking from './pages/products/WasteTracking'
+import VehicalTracking from './pages/products/VehicalTracking'
+
 const App = () => {
-  useEffect(()=>{
+  useEffect(() => {
     // Initialize Lenis
-const lenis = new Lenis();
+    const lenis = new Lenis();
 
-// Listen for the scroll event and log the event data
-lenis.on('scroll', (e) => {
-});
+    // Listen for the scroll event and log the event data
+    lenis.on('scroll', (e) => {
+    });
 
-// Use requestAnimationFrame to continuously update the scroll
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
+    // Use requestAnimationFrame to continuously update the scroll
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
 
-requestAnimationFrame(raf);
-
+    requestAnimationFrame(raf);
   })
+
   return (
     <> 
       <Navbar />
       <div className='bg-back text-head'>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/services' element={<Services />} />
-        <Route path='/solutions' element={<Solutions />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/services/gps' element={<Gps />} />
-        <Route path='/services/fuel-monitor' element={<FuelMonitor />} />
-        <Route path='/services/cctv' element={<Cctv />} />
-        <Route path='/services/asset-tracking' element={<AssetTracking />} />
-        <Route path='/services/vehicle-tracking' element={<VehicalTracking />} />
-        <Route path='/services/waste-tracking' element={<WasteTracking />} />
-      </Routes>
-      <Footer />
-    </div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/solutions' element={<Solutions />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/services/gps' element={<Gps/>} />
+          <Route path='/services/fuel-monitor' element={<FuelMonitor/>} />
+          <Route path='/services/cctv' element={<Cctv />} />
+          <Route path='/services/asset-tracking' element={<AssetTracking />} />
+          <Route path='/services/vehicle-tracking' element={<VehicalTracking />} />
+          <Route path='/services/waste-tracking' element={<WasteTracking />} />
+        </Routes>
+        <Footer />
+      </div>
     </>
   )
 }
